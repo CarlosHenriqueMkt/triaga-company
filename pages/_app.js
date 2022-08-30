97209c36666dd2743ee072573c864d4294431cda
carloshenriquemkt <falacom.ocarlos@gmail.com>
2022-08-30 19:27:15 -0300

    Google Analytics incluso

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.sass";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
+import Script from "next/script";
 
 const colors = {
 	brand: {
@@ -28,6 +30,35 @@ const theme = extendTheme({ colors, typography });
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+			/>
+
+			<Script strategy="lazyOnload" id="gtag">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
+
+			<Head>
+				<title>
+					Triaga Company — Agência de Comunicação e Tecnologia
+				</title>
+				<meta
+					name="description"
+					content="Agência de comunicação e tecnologia"
+				/>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
